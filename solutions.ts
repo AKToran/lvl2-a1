033d995a43d4294dc652d21d83f3d1e8e0a82059
AKToran <aktoran60@gmail.com>
2026-05-09 12:05:39 +0600

    solution to problem 1

--- solutions.ts
+++ solutions.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+type numberArray = number[];
+
+const filterEvenNumbers = (array:numberArray): numberArray=>{
+  let result:numberArray = array.filter((elem:number) => elem % 2 ===0 )
+  return result;
+}
+
